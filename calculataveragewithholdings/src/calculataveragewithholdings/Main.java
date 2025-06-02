c85b9ab1fb4e6fe6a881cdd0a5ab426e19863b50
Jonathan <Jonathangreene8888@gmail.com>
2025-06-01 22:41:42 -0600

    updated method name to include main and updated logic

--- calculataveragewithholdings/src/calculataveragewithholdings/Main.java
+++ calculataveragewithholdings/src/calculataveragewithholdings/Main.java
@@ -2,7 +2,7 @@ package calculataveragewithholdings;
 import java.util.Scanner;  // Import the Scanner class
 
 public class Main {
-	public static void calculataveragewithholdings(String[] args) {
+	public static void main(String[] args) {
 	// Average is the total values added up divided by the number of values
 	// create float variable for tax rate. 
 	// ask for income from user in an integer variable
@@ -22,7 +22,7 @@ public class Main {
 	
 	if(income < 500 ) {
 		taxRate = 0.10f;		
-	} else if (income >= 500 && income <1500) {
+	} else if (income >= 500 && income < 1500) {
 		taxRate = 0.15f;
 	} else if (income >= 1500 && income < 2500) {
 		taxRate = 0.20f;
@@ -32,7 +32,7 @@ public class Main {
 	
 	// Close the scanner
 	myObj.close();
-	System.out.println("Your average weekly tax withholding is: " + income / taxRate);
+	System.out.println("Your average weekly tax withholding is: " + taxRate * income);
 
 	}
 	
